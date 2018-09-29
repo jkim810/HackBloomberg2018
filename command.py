639c26bccb6934f81eca01f7be670a3031dce584
thedavekwon <thedavekwon@gmail.com>
2018-09-28 21:14:38 -0400

    fixed

--- command.py
+++ command.py
@@ -6,28 +6,28 @@ passwd = "diana"
 class Mine:
     def __init__(self, owner, x, y):
         self.owner = owner
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
 class Player:
     def __init__(self, x, y, dx, dy):
-        self.x = x
-        self.y = y
-        self.dx = dx
-        self.dy = dy
+        self.x = float(x)
+        self.y = float(y)
+        self.dx = float(dx)
+        self.dy = float(dy)
 
 class Bomb:
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
 class Wormhole:
     def __init__(self, x, y, r, out_x, out_y):
-        self.x = x
-        self.y = y
-        self.r = r
-        self.out_x = out_x
-        self.out_y = out_y
+        self.x = float(x)
+        self.y = float(y)
+        self.r = float(r)
+        self.out_x = float(out_x)
+        self.out_y = float(out_y)
 
 class STATUS:
     def __init__(self):
@@ -46,50 +46,50 @@ class STATUS:
     
     def parse_info(self, info):
         info = info.split()
-        self.x, self.y, self.dx, self.dy = info[1:5]
-        idx_mine = info.index("MINES")
-        idx_players = info.index("PLAYERS")
-        idx_bombs = info.index("BOMBS")
-        idx_wormholes = info.index("WORMHOLES")
+        self.x, self.y, self.dx, self.dy = map(float, info[1:5])
+        idx_mine = int(info.index("MINES"))
+        idx_players = int(info.index("PLAYERS"))
+        idx_bombs = int(info.index("BOMBS"))
+        idx_wormholes = int(info.index("WORMHOLES"))
         
         #print(idx_mine, idx_players, idx_bombs, idx_wormholes, len(info))
 
         # MINES
-        self.num_mines = info[idx_mine+1]
+        self.num_mines = int(info[idx_mine+1])
         for i in range(idx_mine+2, idx_players, 3):
             self.mines.append(Mine(
                 owner = info[i],
-                x = info[i+1],
-                y = info[i+2]
+                x = float(info[i+1]),
+                y = float(info[i+2])
             ))
         
         # PLAYERS
-        self.num_players = info[idx_players+1]
+        self.num_players = int(info[idx_players+1])
         for i in range(idx_players+2, idx_bombs, 4):
             self.players.append(Player(
-                x = info[i],
-                y = info[i+1],
-                dx = info[i+2],
-                dy = info[i+3]
+                x = float(info[i]),
+                y = float(info[i+1]),
+                dx = float(info[i+2]),
+                dy = float(info[i+3])
             ))
 
         # BOMBS
-        self.num_bombs = info[idx_bombs+1]
+        self.num_bombs = int(info[idx_bombs+1])
         for i in range(idx_bombs+2, idx_wormholes, 2):
             self.bombs.append(Bomb(
-                x = info[i],
-                y = info[i+1]
+                x = float(info[i]),
+                y = float(info[i+1])
             ))
 
         # Wormhole
-        self.num_wormholes = info[idx_wormholes+1]
+        self.num_wormholes = int(info[idx_wormholes+1])
         for i in range(idx_wormholes+2, len(info), 5):
             self.wormholes.append(Bomb(
-                x = info[i],
-                y = info[i+1],
-                r = info[i+2],
-                out_x = info[i+3],
-                out_y = info[i+4]
+                x = float(info[i]),
+                y = float(info[i+1]),
+                r = float(info[i+2]),
+                out_x = float(info[i+3]),
+                out_y = float(info[i+4])
             ))
 
     def receive_info(self):
@@ -100,6 +100,10 @@ class STATUS:
     def receive_scan(self, x, y):
         info = clientpy3.run(id, passwd, "SCAN {} {}".format(x, y))
         #print(info)
+        self.mines = []
+        self.players = []
+        self.bombs = []
+        self.wormholes = []
         self.parse_info(info)
         
 class CONFIGUREATIONS:      
